51b84f86392ce1d3f93987c60815ac8a844f092a
wilmerx5 <wilmercampos2004@gmail.com>
2023-03-31 22:05:13 -0500

    second class homework

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import RUNNING , JUMPING
+from dino_runner.utils.constants import RUNNING , JUMPING, DUCKING
 
 class Dinosaur(Sprite):
     X_POS=80
@@ -15,18 +15,30 @@ class Dinosaur(Sprite):
         self.step_index=0
         self.dino_jump=False
         self.jump_speed=self.JUMP_SPEED
+        self.dino_duck=False
 
     def update(self,user_input):
         if self.dino_run:
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump=True
             self.dino_run=False
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck=True
+            self.dino_run=False
+
         elif not self.dino_jump:
-            self.dino_jump=False
+            # self.dino_jump=False
             self.dino_run=True
+
+        # elif not self.dino_duck:
+        #     self.dino_duck=False
+        #     self.dino_run=True
+
         if self.step_index>10:
             self.step_index=0
 
@@ -49,6 +61,14 @@ class Dinosaur(Sprite):
             self.dino_rect.y=self.Y_POST
             self.dino_jump=False
             self.jump_speed=self.JUMP_SPEED
+
+    def duck(self):
+        self.image=DUCKING[0]if self.step_index<5 else DUCKING[1]
+        self.dino_rect=self.image.get_rect()
+        self.dino_rect.x=self.X_POS
+        self.dino_rect.y=self.Y_POST+22
+        self.step_index+=1
+
 
 
 #agacharse
